4301cf3c2de954909e2d3c904567ccaec0c179cc
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-05-18 21:58:45 +0500

    str_func.py add Title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
-def word_title(word):
+def word_upper(word):
     """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return word.upper()
+
+
+def word_title(word):
+    """делает заглавными первые буквы каждого слова в строке"""
+    return word.title()
